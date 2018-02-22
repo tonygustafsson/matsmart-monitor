ea207de78fc31ffd51a86476e90b5fc596ac0fe9
tonygustafsson <tony.gustafsson@starrepublic.com>
2018-02-22 21:07:27 +0100

    Add views to SQLIte

--- MatSmart.sqlite.sql
+++ MatSmart.sqlite.sql
@@ -9,4 +9,8 @@ CREATE TABLE IF NOT EXISTS `Products` (
 CREATE INDEX IF NOT EXISTS `URLIndex` ON `Products` (
 	`url`
 );
+CREATE VIEW MostDiscounted AS
+SELECT name, url, price, normalPrice, (100 - ((price / normalPrice) * 100)) AS Discount FROM Products ORDER BY Discount DESC;
+CREATE VIEW CheapestProducts AS
+SELECT * FROM Products ORDER BY price ASC;
 COMMIT;
--- scrape.js
+++ scrape.js
@@ -150,7 +150,7 @@ const scrapeSites = products => {
 
         if (message === '') return;
 
-        //sendFBMessage(message);
+        sendFBMessage(message);
     }
     catch (err) {
         console.log(err);
